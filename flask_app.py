418ed8e9f048c4a9d36bf17a645ff539e559336e
stevee-bee <77158150+stevee-bee@users.noreply.github.com>
2021-10-20 22:51:05 +0000

    implemented Flask-Bootstrap to base.html, forms, and tables

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,7 @@
 
 # external packages
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +15,7 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
