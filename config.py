ed7988424566c61e89d5161f370e9f83300dcc6d
stevee-bee <77158150+stevee-bee@users.noreply.github.com>
2021-10-22 22:10:44 +0000

    reformatted requirements.txt; moved sensitive from config.py to .env (not included in repo)

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    DEBUG = True
+    # DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username="dse",
-        password="Ap4tW!veyHp35nM5",
-        hostname="dse.mysql.pythonanywhere-services.com",
-        databasename="dse$fma",
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 
 # A database-backed Flask website to track and manage field crops.
 # based on reference article: https://blog.pythonanywhere.com/121/
+# and Miguel's Microblog Mega-Tutorial: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 # modified for DSE
 
 # external packages
@@ -10,17 +11,24 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+# not used from Miguel's Microblog Mega-Tutorial
+# from flask_babel import Babel, lazy_gettext as _l
+# from flask_mail import Mail
+# from flask_moment import Moment
+
 # internal modules
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-bootstrap = Bootstrap(app)
-login = LoginManager(app)
-login.login_view = 'login'
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+# mail = Mail(app)
+bootstrap = Bootstrap(app)
+# moment = Moment(app)
+# babel = Babel(app)
 
 import routes, models, errors
 
